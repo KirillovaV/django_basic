18d9e0355226b7a7e1cf6bdd25f21719e2b900f4
KirillovaV <victmanio@yandex.ru>
2021-09-04 16:10:24 +0300

    lesson 3

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
+from mainapp.models import Product, ProductCategory
 
 
 def main(request):
-    return render(request, 'mainapp/index.html', {'title': 'магазин'})
+    title = 'Главная'
+    products = Product.objects.all()
+    content = {'title': title, 'products': products}
+    return render(request, 'mainapp/index.html', content)
 
 
-def products(request):
-    return render(request, 'mainapp/products.html', {'title': 'каталог'})
+def products(request, pk=None):
+    print(pk)
+    title = 'каталог'
+    categories = ProductCategory.objects.all()
+    content = {'title': title, 'categories': categories}
+    return render(request, 'mainapp/products.html', content)
 
 
 def contact(request):
