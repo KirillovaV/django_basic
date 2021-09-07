a37df1b6b77fe67f8b6ad01b0f1eac1dd2f83d0d
KirillovaV <victmanio@yandex.ru>
2021-09-07 17:15:05 +0300

    lesson 4

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -19,11 +19,13 @@ import mainapp.views as mainapp
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/',  include('mainapp.urls', namespace='products')),
     path('contact/', mainapp.contact, name='contact'),
     path('', mainapp.main, name='main'),
+    path('auth/',  include('authapp.urls', namespace='auth')),
 ]
 
 if settings.DEBUG:
