f312dade004b52126aa10c6114f6bff3264a8a10
KirillovaV <victmanio@yandex.ru>
2021-10-13 08:22:19 +0300

    lesson_4(12)

--- geekshop/ordersapp/models.py
+++ geekshop/ordersapp/models.py
@@ -63,3 +63,12 @@ class OrderItem(models.Model):
 
     def get_product_cost(self):
         return self.product.price * self.quantity
+
+    @staticmethod
+    def get_item(pk):
+        return OrderItem.objects.filter(pk=pk).first()
+
+    def delete(self):
+        self.product.quantity += self.quantity
+        self.product.save()
+        super(self.__class__, self).delete()
